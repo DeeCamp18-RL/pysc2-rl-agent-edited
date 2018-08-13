2cb23fc92ae710a3d04ad7ff2c9a016da94c816b
sfx <shangfangxin0341@link.tyut.edu.cn>
2018-08-13 22:43:16 +0800

    update adam opt

--- rl/agent.py
+++ rl/agent.py
@@ -14,8 +14,11 @@ class A2CAgent:
         loss_fn, self.loss_inputs = self._loss_func()
 
         self.step = tf.Variable(0, trainable=False)
-        opt = tf.train.RMSPropOptimizer(learning_rate=lr, decay=0.99, epsilon=1e-5)
-        # opt = tf.train.AdamOptimizer(learning_rate=lr, epsilon=1e-5)
+        
+        if config.map == "FindAndDefeatZerglings":
+            opt = tf.train.AdamOptimizer(learning_rate=lr, epsilon=1e-5)
+        else:
+            opt = tf.train.RMSPropOptimizer(learning_rate=lr, decay=0.99, epsilon=1e-5)
         self.train_op = layers.optimize_loss(loss=loss_fn, optimizer=opt, learning_rate=None, global_step=self.step, clip_gradients=clip_grads)
         self.sess.run(tf.global_variables_initializer())
 
